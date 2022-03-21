af05192df3c9a51042661c568b6ca6215d14419d
hnsreeny <hnsreeny@gmail.com>
2022-03-21 11:56:56 +0100

    JS 101

--- mar/21-03/index.js
+++ mar/21-03/index.js
@@ -1,3 +1,4 @@
+// DRY Don't repeat yourself
 // Comment
 console.log(2==3);
 //   ; , - _ ? !  " "  ' '  ` ` § $  &  /  |  \ = : * + { } [ ] # @  ^ ~ % < > () .  
@@ -7,9 +8,37 @@ console.log("Hadi");
 console.log(1<44);
 // true & false
 console.log(1+1);
-
+// 
 // Object
 // Boolean
 // String
 // Number
 // null
+// 
+let x = 0;
+// 
+
+let str = "Hadi";
+let bool = true;
+let empty = null;
+
+
+//  undefined 
+//  not defined
+let cool = "You are cool";
+//  x = 44;
+console.log(x)
+console.log(x * 55 + x - 44);
+//          44 * 55 + 44 - 44
+// empty = 9;
+console.log(empty );
+
+// console.log(typeof );
+
+function iDoSomething (){
+    // function scope
+    console.log("Hey, I'm a function");
+}
+iDoSomething();
+
+iDoSomething();
