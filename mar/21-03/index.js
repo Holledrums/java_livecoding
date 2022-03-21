f714302dd2e61acbb9542c3c09dc4db7c18d8e65
hnsreeny <hnsreeny@gmail.com>
2022-03-21 11:01:06 +0100

    JS 101

---
+++ mar/21-03/index.js
@@ -0,0 +1,11 @@
+// Comment
+console.log(2==3);
+//   ; , - _ ? !  " "  ' '  ` ` § $  &  /  |  \ = : * + { } [ ] # @  ^ ~ % < > () .  
+// Ausdruck mit log Methode
+console.log("Hadi");
+// String
+console.log(1<44);
+// true & false
+console.log(1+1);
+// Number
+// Object
