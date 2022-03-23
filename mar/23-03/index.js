b3d7863630f656c289dd4f194070b946ede303d2
hnsreeny <hnsreeny@gmail.com>
2022-03-23 09:22:44 +0100

    JS 101

--- mar/23-03/index.js
+++ mar/23-03/index.js
@@ -1,3 +1,18 @@
+//  
+console.log("hi HaDI".toLowerCase());
+console.log("hi HaDI".toUpperCase());
+console.log("     Cool    ".trim());
+console.log("     Cool    ".trimStart());
+console.log("     Cool    ".trimEnd());
+console.log("Hi, this is cool".substring(3,9)); 
+console.log("I'm Hadi".includes("@"));
+let num = 0;
+num++;
+console.log(num);
+num--;
+console.log(num);
+
+
 
 // bracket notation [0]
 
