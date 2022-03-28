b9c397c9eef1158125f1ef7c7ad18ee73f02b591
hnsreeny <hnsreeny@gmail.com>
2022-03-28 11:17:26 +0200

    Math

--- mar/28-03/index.js
+++ mar/28-03/index.js
@@ -35,6 +35,35 @@ console.log("max", Math.max(num1, num2, num3)); // 44
 // Math.min()
 console.log("min", Math.min(num1, num2, num3)); // 2
 // Math.random()
-console.log(Math.floor(Math.random() * 5) + 1); // 1 - 6
+console.log(Math.floor(Math.random() * 6) + 1); // 1 - 6
+// console.log(Math.floor(Math.random() * 10) + 1);
+// toFixed()
+let num4 = 22.0000004;
+console.log(num4.toFixed(2)); // 22.44
+let num5 = "1.25e+4";
+console.log(parseFloat(num5));
+//           0123456789
+let alpha = "ABCD";
+console.log(alpha[Math.floor(Math.random() * alpha.length)]);
+// loops
+// for (start; condition ; increment){
+// What will happen
+// }
+// i == index
+// for (let i =0; i <= 10 ; i= i+1)
+//for (let i =0; i <= 10 ; i+= 1)
+//  i = 0
+for (let i = 0; i <= 10; i++) {
+  console.log("i now is", i);
+}
+console.log("Coo");
+// 1 * 2 = 2
+// 2 * 2 = 4
+// 3 * 2 = 6
+// ----
+// 10 * 2 = 20
+for (let i = 1; i <= 10; i++) {
+  console.log(`${i} * 2 = ${i * 2} `);
+}
 
-// Warum random ist nur 0-1
+// Warum random gibt nur 0-1 ?
