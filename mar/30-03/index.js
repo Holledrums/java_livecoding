044458332300e552003baa572bcf99b30bd14770
hnsreeny <hnsreeny@gmail.com>
2022-03-30 10:23:06 +0200

    Array && Loops

--- mar/30-03/index.js
+++ mar/30-03/index.js
@@ -17,6 +17,43 @@ console.log(arr2.length);
 arr2[1] = "Alex";
 arr2.reverse();
 console.table(arr2);
+let userName = arr2[0];
+console.log(userName);
+
+console.log(arr2.indexOf("Hadi"));
+// console.log(arr2.lastIndexOf("Alex"));
+
+// No Duplicates! A set is a collection of unique items. A set can be formed from an array by removing all duplicate items. Create a program which transforms an array into a set of unique values. See the examples below. Example:
+// [1, 4, 4, 7, 7, 7] ➞ [1, 4, 7]
+
+// [1, 6, 6, 9, 9] ➞ [1, 6, 9]
+
+// [2, 2, 2, 2, 2, 2] ➞ [2]
+
+// [5, 10, 15, 20, 25] ➞ [5, 10, 15, 20, 25]
+
+// function clearUp(arr) {
+//   let resultArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     // if (resultArr.indexOf(arr[i]) == -1) {
+//     //   resultArr.push(arr[i]);
+//     // }
+//   }
+//   return console.table(resultArr);
+// }
+function clearUp(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    // ! not
+    if (!result.includes(arr[i])) {
+      result.push(arr[i]);
+    }
+  }
+  return console.table(result);
+}
+
+clearUp(["Ali", "Ali", "Steel"]);
+clearUp([1, 2, 2, 2, 2, 2, 2, 2]);
 
 // splice()
 
