45b3ea66354d69cedbefc26cb25578f124590d75
hnsreeny <hnsreeny@gmail.com>
2022-03-30 11:23:30 +0200

    Array && Loops

--- mar/29-03/index.js
+++ mar/29-03/index.js
@@ -190,14 +190,3 @@ function findGreatest(arr, num) {
 findGreatest([2, 4, 6, 7], 5);
 findGreatest([0, 10, 3], 4);
 findGreatest([10, 20, 30], 12);
-// . Dictionary. Create a function that takes an initial string and an array of words, and returns a filtered array of the words that start with the same letters as the initial string.
-
-// Notes:
-
-// If none of the words match, return an empty array.
-// Keep the filtered array in the same relative order as the original array of words.
-// Examples:
-
-// dictionary("bu", ["button", "breakfast", "border"]) ➞ ["button"]
-// dictionary("tri", ["triplet", "tries", "trip", "piano", "tree"]) ➞ ["triplet", "tries", trip"]
-// dictionary("beau", ["pastry", "delicious", "name", "boring"]) ➞ []
--- mar/30-03/index.js
+++ mar/30-03/index.js
@@ -56,13 +56,61 @@ clearUp(["Ali", "Ali", "Steel"]);
 clearUp([1, 2, 2, 2, 2, 2, 2, 2]);
 
 // splice()
-
+//            0     1        2
+const days = ["Mon", "Mitt", "Don", "Frei"];
+let deletedItems = days.splice(0, 2);
+console.log(deletedItems);
+console.table(days);
 // slice()
+//                  0          1        2       3
+const cities = ["Berlin", "London", "Dubai", "LA"];
+// let newArr = cities; // not a copy
+let newArr = cities.slice(0);
+console.log(newArr);
+console.log(cities);
+
+// Js {}
+// arr []
+// function ()
 
 // split()
+let str = "This is a long text";
+console.table(str.split(" "));
 
 // join()
+const text = ["Hi", "I", "am", "Hadi"];
+console.log(text.join(" "));
 
+// . snake_case ➞ camelCase Create a function toCamelCase() that takes a single string in snake_case and converts it into camelCase.
+
+// Examples:
+
+// toCamelCase("hello_world") ➞ "helloWorld"
+// toCamelCase("javascript_is_fun") ➞ "javascriptIsFun"
+// DRY
+function toCamelCase(str) {
+  let result = "";
+  let newArr = str.toLowerCase().split("_");
+  // console.log(newArr);
+  //   newArr[0] = newArr[0].toLowerCase();
+  for (let i = 1; i < newArr.length; i++) {
+    newArr[i] = newArr[i][0].toUpperCase() + newArr[i].substring(1); //.toLowerCase();
+  }
+  result = newArr.join("");
+  return console.log(result);
+}
+toCamelCase("hello_world");
+toCamelCase("JAVascript_is_fun");
 // concat()
 
-//
+// . Dictionary. Create a function that takes an initial string and an array of words, and returns a filtered array of the words that start with the same letters as the initial string.
+
+// Notes:
+
+// If none of the words match, return an empty array.
+// Keep the filtered array in the same relative order as the original array of words.
+// Examples:
+
+// dictionary("bu", ["button", "breakfast", "border"]) ➞ ["button"]
+// dictionary("tri", ["triplet", "tries", "trip", "piano", "tree"]) ➞ ["triplet", "tries", trip"]
+// dictionary("beau", ["pastry", "delicious", "name", "boring"]) ➞ []
