c95f16c9ed118825af6e540dc226681e7c1bf0e1
hnsreeny <hnsreeny@gmail.com>
2022-03-31 09:57:14 +0200

    Overview

--- mar/30-03/index.js
+++ mar/30-03/index.js
@@ -150,4 +150,41 @@ dictionary("beau", ["pastry", "delicious", "name", "boring"]);
 
 // console.log("Hadi".substring(0, 2));
 
+// Hello Alex. Create an array filled with your friends' and family's names. Loop over the array and greet each friend. Bonus: Print the indexes of each item in the array. Examples:
+// [Maria, Mike, Paul, Doven] ➞ expected output: "Hello Maria! Hello Mike! Hello Paul! Hello Doven!"
+
+// function, string, for
+// Old school
+function greeting(arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `Hello ${arr[i]}! `;
+  }
+  return console.log(str);
+}
+// Es6
+const greeting2 = (arr) => {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `Hello ${arr[i]}! `;
+  }
+  return console.log(str);
+};
+greeting(["Maria", "Mike", "Paul", "Doven"]);
+greeting2(["Maria", "Mike", "Paul", "Doven"]);
+
 // sumOfNumbers. Create a program that adds up the numbers in an array (of at least 3 numbers). Bonus: Print to screen both the sum and the product of these numbers.
+// function arr loop number sum
+// Old school
+function sumOfNumbers(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return console.log(sum);
+}
+
+sumOfNumbers([2, 3, 5]); // 10
+sumOfNumbers([1, 2, 4]); // 7
+sumOfNumbers([1, 4]); // 5
+// Es6
