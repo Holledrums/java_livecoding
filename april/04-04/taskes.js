ceb72926a3582b456f5e86eacb2aa6a636bbbc6e
hnsreeny <hnsreeny@gmail.com>
2022-04-04 13:13:52 +0200

    Functions in js

--- april/04-04/taskes.js
+++ april/04-04/taskes.js
@@ -21,3 +21,14 @@
 // Bonus: Accept floating point values for amount per day, and round the result to a round number.
 
 // ------------------------
+// The Temperature Converter
+// It's hot out! Let's make a converter based on the steps here.
+
+// Create a function called celsiusToFahrenheit:
+
+// Store a celsius temperature into a variable.
+// Convert it to fahrenheit and output "NN°C is NN°F".
+// Create a function called fahrenheitToCelsius:
+
+// Now store a fahrenheit temperature into a variable.
+// Convert it to celsius and output "NN°F is NN°C."
