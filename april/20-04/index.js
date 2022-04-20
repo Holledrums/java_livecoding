19e35551c01b3434a61774ba2628f7cfbdb71b1f
hnsreeny <hnsreeny@gmail.com>
2022-04-20 10:00:44 +0200

    Objects

--- april/20-04/index.js
+++ april/20-04/index.js
@@ -1,9 +1,54 @@
 // Objects
 // const <namespace> = { <key 1>: <value 1>, <key 2>: <value 2>, ... }
 // keys, properties : value
+let str = "";
+let num = 0;
+let bool = true;
+const arr = [];
+const sumFunction = () => {};
+const personObj = {
+  firstName: "Hadi",
+  lastName: "Nsreeny",
+  age: 32,
+  add: "Berlin",
+  toolStack: ["JavasScript", "Java", "PHP"],
+  hasKids: false,
+};
+const personObj2 = {
+  firstName: "Ali",
+  lastName: "Steel",
+  age: 22,
+  add: "LA",
+  toolStack: ["C#", "C++"],
+  hasKids: true,
+};
+const personObj3 = {
+  firstName: "Nancy",
+  lastName: "Schmidt",
+  age: 42,
+  add: "Paris",
+  toolStack: ["Js", "C"],
+  hasKids: true,
+};
+console.log(typeof personObj);
+const carObj = {
+  model: "Bugatti",
+  color: "red",
+  price: 3987000,
+  fuelConsumption: "16.6–30.3 L/100km",
+};
+// method 1
+console.log(personObj["age"]);
+// method 2
+console.log(personObj.toolStack[2]);
 
 // Object Literal
-
+const obj = {};
+console.log(obj);
+obj["name"] = "Olga";
+console.log(obj);
+obj.name = "Jenna";
+console.log(obj);
 // Accessing properties
 // 1- with (bracket notation)
 
