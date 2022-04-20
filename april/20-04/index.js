88fada177d3423eabe3033f1aaa58eebb906f049
hnsreeny <hnsreeny@gmail.com>
2022-04-20 08:51:33 +0200

    Objects

---
+++ april/20-04/index.js
@@ -0,0 +1,30 @@
+// Objects
+// const <namespace> = { <key 1>: <value 1>, <key 2>: <value 2>, ... }
+// keys, properties : value
+
+// Object Literal
+
+// Accessing properties
+// 1- with (bracket notation)
+
+// 2- with (dot notation)
+
+// Assignment using dot notation, more used to access a property value
+
+// Assigning using bracket notation, more used for an empty object
+
+// Tell us more about you
+
+// Object.keys
+
+// Object.values
+
+// Object.assign
+
+// Object.defineProperty()
+
+// Object.defineProperties()
+
+// Object.hasOwnProperty()
+
+// Object.seal()
