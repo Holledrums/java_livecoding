6e225232e46adaa759ef5300e3e8f89f65763fb0
hnsreeny <hnsreeny@gmail.com>
2022-04-21 10:27:19 +0200

    Objects

--- april/21-04/index.js
+++ april/21-04/index.js
@@ -20,12 +20,58 @@ const cars = { number: 5, storage: 4 };
 const house = { city: "Berlin", add: "xxx str" };
 const myData = Object.assign(cars, house, { name: "Me", age: 99 });
 console.log(myData);
+// seal
 Object.seal(myData);
+// isSealed
 console.log(Object.isSealed(myData));
 delete myData.name;
 console.log(myData);
-// Object.defineProperty()
 
+// Keys
+const objKeys = Object.keys(myData);
+console.log(objKeys);
+// Values
+const objValues = Object.values(myData);
+console.log(objValues);
+
+/*
+
+[]
+()
+{}
+key : "value",
+
+[{},{},{},{},{}]
+
+console.log();
+
+
+*/
+const ppl = [
+  { name: "Ali", age: 22 },
+  { name: "Nancy", age: 24 },
+  { name: "Steel", age: 32 },
+  { name: "Olga", age: 40 },
+];
+console.log(ppl[1].name);
+console.log(ppl[1]["name"]);
+
+// Object.defineProperty()
+const userInfoObj = {};
+
+Object.defineProperty(userInfoObj, "name", {
+  value: "Hadi",
+  writable: false,
+});
+userInfoObj["add"] = "Berlin";
+console.log(userInfoObj);
+userInfoObj.name = "Steel";
+console.log(userInfoObj.name);
 // Object.defineProperties()
+Object.defineProperties(userInfoObj, {
+  lastName: { value: "Nsreeny", writable: true },
+  act: { value: ["Cooking", "Biking"] },
+  bikes: { value: ["V2"], writable: false },
+});
 
 // Object.hasOwnProperty()
