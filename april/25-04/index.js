d453d293ca206e78fbdc95fe0499c5f35e13bd75
hnsreeny <hnsreeny@gmail.com>
2022-04-25 10:38:53 +0200

    Loops in js

--- april/25-04/index.js
+++ april/25-04/index.js
@@ -39,9 +39,12 @@ const programming = {
   difficulty: 8,
 };
 // Write the command to add the language "Go" to the end of the languages array.
-
+programming.languages.push("Go");
+console.log(programming.languages);
 // Using a loop, console.log all of the values in the programming object.
-
+for (const key in programming) {
+  console.log(programming[key]);
+}
 // Write a command to add a new key called isFun and a value of true to the programming object.
 
 // Using a loop, iterate through the languages array and console.log all of the languages.
