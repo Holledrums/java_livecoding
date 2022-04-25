5e6c2a7e4581993d96d7f743e22665e9676f7a21
hnsreeny <hnsreeny@gmail.com>
2022-04-25 13:08:12 +0200

    Loops in js

--- april/25-04/tasks.js
+++ april/25-04/tasks.js
@@ -5,3 +5,8 @@
 // freeShipping({ "Sponge": 3.50, "Soap": 5.99 }) ➞ false
 // freeShipping({ "Surround Sound Equipment": 499.99 }) ➞ true
 // freeShipping({ "Wool": 13.99, "Knitting Needles": 15.50, "Bag": 13.99 }) ➞ false
+
+// MakePairs - Write a method that returns a deep array like [[key, value]]
+// Task description: Write a method that returns a deep array like [[key, value]]
+
+// Expected Result: ({ a: 1, b: 2 }) => [['a', 1], ['b', 2]]
