853956ed77aac72fd0a18786eb1d7b1933e0e122
hnsreeny <hnsreeny@gmail.com>
2022-04-25 09:40:00 +0200

    Loops in js

--- april/25-04/index.js
+++ april/25-04/index.js
@@ -1,8 +1,27 @@
 // Object
+const personObj = {
+  firstName: "Ali",
+};
+personObj.display = function () {
+  console.log(`Hey ${this.firstName}`);
+};
+// [{},{},{}]
 
+//personObj.display();
 // For
-
+const names = ["Ali", "Nancy", "Olga", "Steel"];
+// for (let i = 0; i < names.length; i++) {
+//   console.log(`${names[i]}`);
+// }
+const steelObj = {
+  name: "Steel",
+  age: 22,
+  add: "Paris",
+};
 // For in
+for (const key in steelObj) {
+  console.log(`${key} ❤️ :  Hey ${steelObj[key]} `);
+}
 
 // for of
 
