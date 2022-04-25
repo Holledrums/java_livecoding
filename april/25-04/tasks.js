460a888555188c441226f360d149eefc2a1f4116
hnsreeny <hnsreeny@gmail.com>
2022-04-25 13:15:42 +0200

    Loops in js

--- april/25-04/tasks.js
+++ april/25-04/tasks.js
@@ -10,3 +10,50 @@
 // Task description: Write a method that returns a deep array like [[key, value]]
 
 // Expected Result: ({ a: 1, b: 2 }) => [['a', 1], ['b', 2]]
+
+// Object Person. Create an object named person. Loop through the object and print both the property and value of the object.
+
+// 2. Get Values. Create a function that returns an array of all values of an object's properties.
+
+// Examples:
+// getObjectValues({
+//   choice1: "tea",
+//   choice2: "coffee",
+//   choice3: "milk"
+// })
+// Expected output:
+// ["tea", "coffee", "milk"]
+
+// 3. Add A Method. Create an object and add a method to that object which prints the values of the objects in a string.
+
+// Example
+// let person = {
+//   firstName: "Michael",
+//   lastName: "Smith",
+//   job: "driver",
+//   age: 20,
+//   city: "Paris"
+// }
+// Example of Expected Output "Michael Smith is a 20 year old driver in Paris".
+
+// 1. Convert keys and values into an array. Create a function that converts an object into an array of keys and values.
+
+// Examples:
+// objectToArray({
+//   A: 1,
+//   B: 2,
+//   C: 3
+// })
+// Expected output:
+// [["A", 1], ["B", 2], ["C", 3]]
+
+// List Properties. Create a function that returns an array of properties of a javascript object.
+
+// Example
+// let student = {
+//   name: "Mike",
+//   class: "4A"
+//   course: "English"
+// }
+// Expected output:
+// ["name", "class", "course"]
