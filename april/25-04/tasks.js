9a266df61f29b72d4cecc12426ca02e5415e66bc
hnsreeny <hnsreeny@gmail.com>
2022-04-25 15:32:02 +0200

    Loops in js

--- april/25-04/tasks.js
+++ april/25-04/tasks.js
@@ -5,13 +5,48 @@
 // freeShipping({ "Sponge": 3.50, "Soap": 5.99 }) ➞ false
 // freeShipping({ "Surround Sound Equipment": 499.99 }) ➞ true
 // freeShipping({ "Wool": 13.99, "Knitting Needles": 15.50, "Bag": 13.99 }) ➞ false
+// function return (true/false), if sum > 50, Object.values, loop
+// Op1
+// function freeShipping(obj) {
+//   let sum = 0;
+//   const valuesArray = Object.values(obj);
+//   for (let i = 0; i < valuesArray.length; i++) {
+//     sum += valuesArray[i];
+//   }
+//   return console.log(sum > 50);
+// }
+//  OP prof
+// const freeShipping = (obj) => Object.values(obj).reduce((acc, cur) => acc + cur) > 50;
+// [ 3.5, 5.99 ].reduce((acc, cur) => acc + cur) > 50;
+// [ 3.5, 5.99 ]         ->         9.5          > 50
+// Op3
+const freeShipping = (obj) => {
+  const valueArr = Object.values(obj);
+  const sum = valueArr.reduce((acc, cur) => acc + cur);
+  return sum > 50;
+};
+// [ 3.5, 5.99 ].reduce((acc, cur) => acc + cur) > 50;
+// [ 3.5, 5.99 ]         ->         9.5          > 50
 
+console.log(freeShipping({ Sponge: 3.5, Soap: 5.99 }));
+console.log(freeShipping({ "Surround Sound Equipment": 499.99 }));
 // MakePairs - Write a method that returns a deep array like [[key, value]]
 // Task description: Write a method that returns a deep array like [[key, value]]
 
 // Expected Result: ({ a: 1, b: 2 }) => [['a', 1], ['b', 2]]
+const makePairs = { a: 1, b: 2, objToArr: () => Object.entries(makePairs) };
+
+console.log(makePairs.objToArr());
 
 // Object Person. Create an object named person. Loop through the object and print both the property and value of the object.
+const person = {
+  firstName: "Nancy",
+  lastName: "Schmidt",
+  age: 34,
+};
+for (const key in person) {
+  console.log(`${key} : ${person[key]}`);
+}
 
 // 2. Get Values. Create a function that returns an array of all values of an object's properties.
 
