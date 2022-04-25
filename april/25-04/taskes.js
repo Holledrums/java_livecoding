e78bcfef2ea60b31599ddbe3b8f47b07b4eac50e
hnsreeny <hnsreeny@gmail.com>
2022-04-25 13:03:43 +0200

    Loops in js

---
+++ april/25-04/taskes.js
@@ -0,0 +1,7 @@
+// 5. Free Shipping. Create a function that determines whether an online order should get free shipping. An order gets free shipping if the total cost of items exceeds €50.
+
+// Examples:
+
+// freeShipping({ "Sponge": 3.50, "Soap": 5.99 }) ➞ false
+// freeShipping({ "Surround Sound Equipment": 499.99 }) ➞ true
+// freeShipping({ "Wool": 13.99, "Knitting Needles": 15.50, "Bag": 13.99 }) ➞ false
