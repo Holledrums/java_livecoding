9c38c1a89f4f2876214a502b5d9e98df74b4f6ab
hnsreeny <hnsreeny@gmail.com>
2022-04-27 09:54:01 +0200

    Advanced Array methods

--- april/27-04/index.js
+++ april/27-04/index.js
@@ -1,10 +1,28 @@
-const emoji = ["😉", "😉", "😉", "😉", "😎"];
+const emojis = ["😉", "😉", "😉", "😉", "😎", "😎", "🤓"];
+const discounts = [22, 3, 45, 6];
 // map
 // Have 4 toDos
 // 1. method
 // 2. callBack
 // 3. loop
 // 4. make a copy for the original array
+const emojiCopy = emojis.map((emoji, index) => {
+  return `hi ${emoji} ${index}`;
+});
+console.log(emojiCopy);
+const copyDiscounts = discounts.map((discount) => discount * 22);
+console.log(copyDiscounts);
+const users = [
+  { id: "john", name: "John Smith", age: 20 },
+  { id: "ann", name: "Ann Smith", age: 24 },
+  { id: "pete", name: "Pete Peterson", age: 31 },
+];
+// const greeting = users.map((user) => `Hey ${user.name}, ${user.age} years old`);
+const greeting = users.map((user) => {
+  return { something: `Hey ${user.name}, ${user.age} years old` };
+});
+console.log(greeting);
+// const yorNewCopy = yourOriginalArray.map((element,index)=>{return "your new index value"});
 
 // filter
 
