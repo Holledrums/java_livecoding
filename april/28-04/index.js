79494545bdd960198220973069c38cc1f98f42a9
hnsreeny <hnsreeny@gmail.com>
2022-04-28 10:13:38 +0200

    Js overview

--- april/28-04/index.js
+++ april/28-04/index.js
@@ -1,22 +1,38 @@
 // for
 // i -> index
-for (let i = 0; i <= 10; i++) {
-  //  console.log(i);
+console.log("----- for ------");
+for (let i = 0; i == 10; i++) {
+  console.log(i);
 }
 const personObj = {
   firstName: "Steel",
   age: 22,
 };
-for (const prop in personObj) {
-  console.log(`${prop} : ${personObj[prop]}`);
+console.log("---- For in -----");
+for (const key in personObj) {
+  console.log(`${key} 😎 ${personObj[key]}`);
 }
 const namesArr = ["Nancy", "Ali", "Olga", "Steel"];
+console.log("------ For of ------");
 for (const name of namesArr) {
   console.log(name);
 }
+console.log("------ do while ------");
 // do while
+let i = 10;
+do {
+  console.log(i);
+  i++;
+} while (i < 10);
+console.log(i);
 
+console.log("------ while ------");
 // while
+let index = 0;
+while (index < 5) {
+  console.log(index);
+  index++;
+}
 
 // forEach
 
@@ -27,3 +43,5 @@ for (const name of namesArr) {
 // some
 
 // every
+
+// = == ===
