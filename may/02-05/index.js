5c5ad4b1e4f43a5ae1589574b80f45e8755498b5
hnsreeny <hnsreeny@gmail.com>
2022-05-02 13:26:02 +0200

    Js overview

--- may/02-05/index.js
+++ may/02-05/index.js
@@ -63,3 +63,30 @@ for (let i = 0; i < 10; i++) {
 }
 
 // switch
+let color = "blue";
+// () [] {}
+let resultText = "";
+switch (color) {
+  case "blue":
+  case "lightBlue":
+    // do something
+    resultText = `This is ${color} `;
+    break;
+  case "green":
+  case "light green":
+  case "dark green":
+    // do something
+    resultText = `This is ${color} `;
+    break;
+  case "black":
+    // do something
+    resultText = `This is ${color} `;
+    break;
+  case "red":
+    // do something
+    resultText = `This is ${color} `;
+    break;
+  default:
+    resultText = "Sorry this color is not in our list";
+}
+console.log(resultText);
