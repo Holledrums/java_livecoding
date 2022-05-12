73992bf71f35bc137a5fe741a2797a3b14b34467
hnsreeny <hnsreeny@gmail.com>
2022-05-12 11:13:43 +0200

    Extends && super

--- may/11-05/index.js
+++ may/11-05/index.js
@@ -171,3 +171,44 @@ console.log(samsungWM.program);
 samsungWM.randomProgram();
 console.log(samsungWM.program);
 samsungWM.programCheck();
+
+// Creat a constructor function that receive product details (productName,prise,inventory) then build a method to display this data in clear outputted form
+// build two product using your constructor function
+// i.e : (Pepsi,2.33,2232) -> Form item Pepsi selling prise 2.33€. we have 2232 in our store
+// other one later
+
+function Product(productName, prise, inventory) {
+  this.productName = productName;
+  this.prise = prise;
+  this.inventory = inventory;
+  this.showInfo = function () {
+    console.log(
+      `Form item ${this.productName} selling prise ${this.prise}€. we have ${this.inventory} in our store`
+    );
+  };
+}
+const drink = new Product("Pepsi", 2.33, 2232);
+drink.showInfo();
+
+// get & set, create a car object that uses getter and setter method to get and set the following (modelName,modelNumber,enginCapacity)
+// first get the data
+// then set the data
+
+const car = {
+  modelName: "Audi",
+  modelNumber: "A8",
+  enginCapacity: 500,
+  get data() {
+    console.log(
+      `This ${this.modelName} ${this.modelNumber} has ${this.enginCapacity}.`
+    );
+  },
+  set data(obj) {
+    this.modelName = obj.name;
+    this.modelNumber = obj.number;
+    this.enginCapacity = obj.cap;
+  },
+};
+car.data;
+car.data = { name: "BMW", number: "Z4", cap: 320 };
+car.data;
--- may/12-05/index.js
+++ may/12-05/index.js
@@ -1,4 +1,34 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  printData() {
+    return console.log(`Hey ${this.name} you are ${this.age} years old.`);
+  }
+}
+
+const steelObj = new Person("Steel", 34);
+
+console.log(steelObj);
+steelObj.printData();
 // extends && super
+// extends and super()
+// extends --> inheriting the methods from the Original class which being called
+// super --> gets access to the parent's properties and methods
+class Kind extends Person {
+  constructor(name, age, height = 50) {
+    super(name, age);
+    this.height = height;
+  }
+  show() {
+    return console.log(`this Kind class is extends form Person, ${this.name}`);
+  }
+}
+
+const zainBabyObj = new Kind("Zain", 1, 75);
+zainBabyObj.show();
+zainBabyObj.printData();
 
 // Miserable Parody of a Calculator
 // Create a function that will handle simple math expressions. The input is an expression in the form of a string.
@@ -8,10 +38,86 @@
 // calculator("13+2-5*2") ➞ 5
 // calculator("49/7*2-3") ➞ 11
 
-// 030-1247893 -> I'll call now
+// 030-1247-893 -> I'll call now
+const phoneNumCheck = (str) => {
+  if (str[3] != "-") {
+    return "Sorry this is not correct";
+  }
+  let num = str.replaceAll("-", "");
+  // let numInArr = str.split("-").join("");
+  if (num.length != 10) {
+    return "Sorry, this is not ready num to call";
+  } else if (num.substring(0, 3) != "030") {
+    return "Sorry, this is not Berlin. Your number should start with 030";
+  } else if (num.length == 10 && num.substring(0, 3) == "030") {
+    return "Cool, I'll call";
+  } else {
+    return "Sorry, something went wrong";
+  }
+};
+console.log(phoneNumCheck("030-1247893"));
+console.log(phoneNumCheck("030-1893"));
+console.log(phoneNumCheck("020-1247893"));
 
-// phoneCheck("0110111111");
 // Email check, write a program that checks yours input if it's a correct email
 // emailCheck("Hi@hh.co") -> true
 // emailCheck("Hihh.co") -> false
 // emailCheck("@hh.co") -> false
+
+const validate = (str) => {
+  let at, dot;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "@") at = i;
+    if (str[i] === ".") dot = i;
+  }
+  if (at > 0 && dot > at) {
+    return `${str} is valid.`;
+  } else {
+    return `${str} is invalid`;
+  }
+};
+console.log("------");
+console.log(validate("contact@hadi-nsreeny.com"));
+console.log(validate("@hadi-nsreeny.com@"));
+console.log(validate("t@hadi-nsreenycom"));
+
+const check = (num) => {
+  if (num < 5) {
+    console.log("cool");
+  }
+  if (num == 5) {
+    console.log("This is super cool");
+  }
+  if (num != 5) {
+    console.log("This is Nice");
+  }
+};
+check(4);
+
+// convert arrays into object
+const phoneNames = [
+  "iPhone 13 Pro Max",
+  "Smasung Ultra 22",
+  "iPhone 12 Pro",
+  "LG xxx",
+];
+const phonePrises = [1150, 1200, 900, 560];
+const result = phoneNames.reduce((acc, item, i) => {
+  acc[item] = phonePrises[i];
+  return acc;
+}, {});
+console.log(result);
+
+// Hour Tracking
+const workingTime = [
+  { day: "Mon", start: 8, end: 17 },
+  { day: "Din", start: 9, end: 16 },
+  { day: "Mitt", start: 9, end: 16 },
+  { day: "Don", start: 8, end: 17 },
+  { day: "Fri", start: 8, end: 18 },
+];
+const hadiWorkingHours = workingTime.reduce((acc, day) => {
+  // (end -start)
+  return (acc += day.end - day.start);
+}, 0);
+console.log(hadiWorkingHours);
