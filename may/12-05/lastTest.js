c6181d84c213968d3156e8301e7c9d0e74b2b525
hnsreeny <hnsreeny@gmail.com>
2022-05-12 13:25:32 +0200

    Extends && super

--- may/12-05/lastTest.js
+++ may/12-05/lastTest.js
@@ -34,3 +34,52 @@ console.log(isDivisible(100));
 const isRaining = (bool) => (bool ? "Wed day" : "Dry day");
 console.log(isRaining(true));
 console.log(isRaining());
+
+// ### Loops
+
+//  1. Sequence
+// * Create a function named geometricalSequence and use a loop to print the following sequence: 1 2 4 8 16 32 64 128 256 . Concatenate each value to a string and return a string.
+const geometricalSequence = () => {
+  let result = "";
+  for (let i = 1; i <= 256; ) {
+    result += `${i} `;
+    i *= 2;
+  }
+
+  return result;
+};
+console.log(geometricalSequence());
+//  2. Multiples
+// * Create a function named multiplesOfThree and use a loop to print the first five multiples of three: 3 6 9 12 15 . Concatenate each value to a string and return a string.
+
+const multiplesOfThree = () => {
+  let result = "";
+  for (let i = 1; i <= 5; i++) {
+    result += `${i + 3} `;
+  }
+  return result;
+};
+console.log(multiplesOfThree());
+// ### Problem Solving
+
+//  1. How many?
+// * Create a function named vowelCount that accepts a string as an argument. Check how many vowels the string contains, if any. Return the vowel count of the string.
+//     > **Examples**:
+//
+//       vowelCount("hello") // 2
+//       vowelCount("test") // 1
+//       vowelCount("fbw") // 0
+//
+const vowelCount = (str) => {
+  // let newStr = str.toLowerCase();
+  let counter = 0;
+  let vowels = "aeiouAEIOU";
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      counter++;
+    }
+  }
+  return counter;
+};
+console.log(vowelCount("hello"));
+console.log(vowelCount("fbw"));
